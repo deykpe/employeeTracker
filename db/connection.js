0cf1ac0d76f46200064b3a3e7c0abee88c64f868
David Pang <dpang2@uci.edu>
2019-12-11 17:22:31 -0800

    modified connection method

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,6 @@
 const util = require("util");
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  // Your username
-  user: "root",
-  // Your password
-  password: "2890768@Bc",
-  database: "employees"
-});
+
 connection.connect();
 // Setting up connection.query to use promises instead of callbacks
 // This allows us to use the async/await syntax
